167934a3ed4e6074abc696ba8945fdd2f9684f47
namanathate220212-design <namanathate220212@gmail.com>
2026-06-24 17:12:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,9 +20,9 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6907945708').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/naman_athate") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003899511245'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/c/3899511245/3')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003875184135').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://namanathate220212_db_user:<*Naman220212>@skyflixprimebot.7llgqv5.mongodb.net/?appName=SkyFlixPrimeBot")
